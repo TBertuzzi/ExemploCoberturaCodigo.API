4323a3310b3176d08e2ad305a615c48ae4ea7f7c
Thiago Bertuzzi <thiago.bertuzzi@gmail.com>
2023-03-03 10:24:41 -0300

    :sparkles: Adicionado Mais Exemplos

--- ExemploCoberturaCodigo.Domain.Tests/ComprasServiceTests.cs
+++ ExemploCoberturaCodigo.Domain.Tests/ComprasServiceTests.cs
@@ -2,19 +2,25 @@ using ExemploCoberturaCodigo.Data.Repositories;
 using ExemploCoberturaCodigo.Domain.Interfaces.Services;
 using ExemploCoberturaCodigo.Domain.Models;
 using ExemploCoberturaCodigo.Domain.Services;
+using Xunit.Abstractions;
+using Xunit.Sdk;
 
 namespace ExemploCoberturaCodigo.Domain.Tests
 {
+    //[Trait("Categoria", "Compras")]
     public class ComprasServiceTests
     {
-        private ComprasService _ComprasService;
+        private readonly ComprasService _ComprasService;
+        private readonly ITestOutputHelper _TestOutputHelper;
 
-        public ComprasServiceTests()
+        public ComprasServiceTests(ITestOutputHelper testOutputHelper)
         {
             _ComprasService = new ComprasService(new ComprasRepository());
+            _TestOutputHelper = testOutputHelper;
         }
 
         [Fact]
+        //[Trait("Categoria","OrdemCompra")]
         public async Task TupleRetornaFalsoSeIdOrdemCompraNaoForFornecido()
         {
             var usuario = new Usuario
@@ -43,13 +49,16 @@ namespace ExemploCoberturaCodigo.Domain.Tests
         }
 
         [Fact]
+        //[Trait("Categoria", "Usuario")]
         public async Task TupleRetornaFalsoSeUsuarioNaoForFornecido()
         {
+            _TestOutputHelper.WriteLine("Usuario não fornecido");
             var retorno = await _ComprasService.AprovarOrdemCompra(1, new Usuario());
             Assert.False(retorno.Item1);
         }
 
         [Fact]
+        //[Fact(Skip = "Essa regra sera validada em outro momento")]
         public async Task TupleRetornaFalsoSeUsuarioNaoTiverPermissao()
         {
             var usuario = new Usuario
@@ -63,6 +72,7 @@ namespace ExemploCoberturaCodigo.Domain.Tests
             Assert.False(retorno.Item1);
         }
 
+        #region Mais Testes
         //[Fact]
         //public async Task TupleRetornaFalsoSeIdusuarioForNegativo()
         //{
@@ -76,6 +86,28 @@ namespace ExemploCoberturaCodigo.Domain.Tests
         //    var retorno = await _ComprasService.AprovarOrdemCompra(1, usuario);
         //    Assert.False(retorno.Item1);
         //}
+        #endregion
+
+        #region Data Driven
+        //[Theory]
+        //[InlineData(1)]
+        //[InlineData(2)]
+        //[InlineData(3)]
+        //[InlineData(4)]
+        //[InlineData(5)]
+        //public async Task TupleRetornaFalsoSeIdsOrdemCompraNaoForemEncontrados(int ordemCompra)
+        //{
+        //    var usuario = new Usuario
+        //    {
+        //        Id = 1,
+        //        Name = "Bertuzzi",
+        //        PermissaoAprovar = true
+        //    };
+
+        //    var retorno = await _ComprasService.AprovarOrdemCompra(ordemCompra, usuario);
+        //    Assert.False(retorno.Item1);
+        //}
+        #endregion
 
     }
 }
